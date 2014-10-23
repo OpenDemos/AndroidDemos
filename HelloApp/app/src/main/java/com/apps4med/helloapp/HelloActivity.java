b0009ffd04075b3f3142762535a27bb38a946bda
iskitsas <iskitsas@gmail.com>
2014-10-23 22:45:24 +0300

    HelloApp with button, editText and AlertDialog

--- HelloApp/app/src/main/java/com/apps4med/helloapp/HelloActivity.java
+++ HelloApp/app/src/main/java/com/apps4med/helloapp/HelloActivity.java
@@ -1,17 +1,30 @@
 package com.apps4med.helloapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class HelloActivity extends Activity {
+    TextView topTextView;
+    EditText editTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+
+        //Set View object
+        topTextView = (TextView) findViewById(R.id.topTextView);
+        editTextView = (EditText) findViewById(R.id.editTextView);
+
     }
 
 
@@ -32,5 +45,25 @@ public class HelloActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void submitButton(View view) {
+        // http://developer.android.com/guide/topics/ui/dialogs.html
+
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(HelloActivity.this);
+
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(editTextView.getText())
+                .setTitle(R.string.app_name);
+        // Add the buttons
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+        // 3. Get the AlertDialog from create()
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
