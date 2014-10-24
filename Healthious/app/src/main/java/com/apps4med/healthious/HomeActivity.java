d6ada94bde556c497213da11e433b9878c5639fa
iskitsas <iskitsas@gmail.com>
2014-10-24 15:23:47 +0300

    Using the support library

--- Healthious/app/src/main/java/com/apps4med/healthious/HomeActivity.java
+++ Healthious/app/src/main/java/com/apps4med/healthious/HomeActivity.java
@@ -1,22 +1,19 @@
 package com.apps4med.healthious;
 
-import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
-import android.content.Context;
-import android.os.Build;
+
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import com.apps4med.healthious.fragments.PlaceholderFragment;
 
+/**
+ * http://developer.android.com/tools/support-library/index.html
+ * */
 
-public class HomeActivity extends Activity implements ActionBar.OnNavigationListener {
+public class HomeActivity extends ActionBarActivity implements ActionBar.OnNavigationListener {
 
     /**
      * The serialization (saved instance state) Bundle key representing the
@@ -30,7 +27,7 @@ public class HomeActivity extends Activity implements ActionBar.OnNavigationList
         setContentView(R.layout.activity_home);
 
         // Set up the action bar to show a dropdown list.
-        final ActionBar actionBar = getActionBar();
+        final ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
 
@@ -89,43 +86,9 @@ public class HomeActivity extends Activity implements ActionBar.OnNavigationList
     public boolean onNavigationItemSelected(int position, long id) {
         // When the given dropdown item is selected, show its contents in the
         // container view.
-        getFragmentManager().beginTransaction()
+        getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
                 .commit();
         return true;
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-            return rootView;
-        }
-    }
-
 }
